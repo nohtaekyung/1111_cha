83e386f7b50ee2d6153dd8ba6df5adc222fd7fb3
hwangsujoung <iove951221@gmail.com>
2019-11-09 17:59:29 +0900

    hsj - 클릭 메뉴 작업

--- js/gnb.js
+++ js/gnb.js
@@ -16,9 +16,12 @@ $(function(){
     $('.gnb_wrap').find('> ul > li').on('click',function(){
         if(!$(this).hasClass('active')){
             $(this).siblings().removeClass('active');
+            $(this).siblings().find('> ul').slideUp();
             $(this).addClass('active');
+            $(this).find('> ul').slideDown();
         }else{
             $(this).removeClass('active');
+            $(this).find('> ul').slideUp();
         }
     })
 })
