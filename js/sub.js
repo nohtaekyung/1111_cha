12d0394ed41ea0cd3e9ceb06ef96a5df47511bcf
hwangsujoung <iove951221@gmail.com>
2019-11-17 21:58:00 +0900

    hsj - 진료과/의료진 작업중

--- js/sub.js
+++ js/sub.js
@@ -141,19 +141,19 @@ $(function() {
     var faqObj = new faqController();
     faqObj.init( '.comment_wrap' , 'list_on');
 
-    if($('.disease_list_con').length != 0){
-        $('.disease_list_con').find('> li > a').on('click',function(){
+    if($('ul[data-wv-slide-list]').length != 0){
+        $('ul[data-wv-slide-list]').find('> li > a').on('click',function(){
             var parent = $(this).parent();
             if(!parent.hasClass('active')){
                 // 클릭 시 다 열리게 할거면 이부분 삭제
                 parent.siblings().removeClass('active');
-                parent.siblings().find('> .list_style_1_con').slideUp();
+                parent.siblings().find('> .desc_con').slideUp(300);
                 // -------------------------------------
                 parent.addClass('active');
-                parent.find('> .list_style_1_con').slideDown();
+                parent.find('> .desc_con').slideDown(300);
             }else{
                 parent.removeClass('active');
-                parent.find('> .list_style_1_con').slideUp();
+                parent.find('> .desc_con').slideUp(300);
             }
         })
     }
