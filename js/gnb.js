a4247b4521ec80cff3b8dad36315c9e3b4a64328
nohtaekyung <tknohh@naver.com>
2019-12-05 21:56:37 +0900

    ntk

--- js/gnb.js
+++ js/gnb.js
@@ -26,3 +26,61 @@ $(function(){
         }
     })
 })
+
+var naviEl = {
+    init : function(){
+        this.el = $('#navigator');
+        this.realEl = $('#navigator').find('> li')[0];
+        this.elHeight = $('#navigator').outerHeight();
+        this.el.css('height', '101px');
+        this.registEvent();
+    }
+
+    ,registEvent : function(){
+        var _this = this;
+        $(this.realEl).on('click', function(){
+            if( $(_this.el).hasClass('active') ){
+                TweenMax.to( _this.el, 0.5, {height : '101px', onComplete : function(){
+                    $(_this.el).removeClass('active');
+                }});
+            }else{
+                TweenMax.to( _this.el, 0.5, {height : _this.elHeight, onComplete : function(){
+                    $(_this.el).addClass('active');
+                }});
+            }
+        });
+
+        $('.sns_btn_con').on('click', function(){
+            if( $('.sns_popup_wrap').hasClass('active') ){
+                $('.sns_popup_wrap').removeClass('active');
+            }else{
+                $('.sns_popup_wrap').addClass('active');
+            }
+        })
+
+        $('.sns_close_btn').on('click', function(){
+            if( $('.sns_popup_wrap').hasClass('active') ){
+                $('.sns_popup_wrap').removeClass('active');
+            }else{
+                $('.sns_popup_wrap').addClass('active');
+            }
+        })
+    }
+
+    ,title_pos : function(){
+        var scrT = $(window).scrollTop();
+        if(scrT > $("header").outerHeight()){
+            $(".wrapper").addClass("fixed");
+        }else{
+            $(".wrapper").removeClass("fixed");
+        }
+    }
+}
+
+$(document).ready(function(){
+    naviEl.init();
+})
+
+$(window).on('scroll', function(){
+    naviEl.title_pos();
+})
